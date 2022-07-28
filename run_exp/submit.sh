419570a0d19c5ed1b92b5c526387bc1cceaca2f6
Yuchen Zeng <yzeng58@wisc.edu>
2022-07-27 20:42:48 -0400

    implemented the clustering method in parallel

--- run_exp/submit.sh
+++ run_exp/submit.sh
@@ -17,5 +17,5 @@
 # python hyperparameter_selection.py -a robust_dro -d waterbirds --wandb_group_name waterbirds_2.0
 
 python hyperparameter_selection.py -a erm -d civilcomments --wandb_group_name civilcomments_1.0
-python hyperparameter_selection.py -a grass -d civilcomments --wandb_group_name civilcomments_1.0
+# python hyperparameter_selection.py -a grass -d civilcomments --wandb_group_name civilcomments_1.0
 python hyperparameter_selection.py -a robust_dro -d civilcomments --wandb_group_name civilcomments_1.0

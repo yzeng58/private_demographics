b18c05d73c91aa3a5bf4637f32d2b840ecfdaa94
Yuchen Zeng <yzeng58@wisc.edu>
2022-09-15 17:42:23 +0000

    update again

--- run_exp/clustering_parameter_selection.py
+++ run_exp/clustering_parameter_selection.py
@@ -25,6 +25,7 @@ def main(args):
     if dataset == 'civilcomments':
         if method == 'eiil':
             queue = 'x86_24h'
+            mem = '64g'
         else:
             queue = 'x86_1h'
         start_model_path = '%s/privateDemographics/models/civilcomments/sgd_m_1_num_epoch_10_batch_size_32_lr_1e-05_optimizer_adam_subsample_0_weight_decay_0.01_best.model'  % root_dir
